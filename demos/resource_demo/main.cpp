d6f6669dccba493cd9e8324b27f2294ca2cf9ed8
Nebojša Koturović <necer95@gmail.com>
2019-03-28 00:27:29 +0100

    Resource manager abstracted and improved

--- demos/resource_demo/main.cpp
+++ demos/resource_demo/main.cpp
@@ -1,28 +1,63 @@
 #include <iostream>
-#include <filesystem>
 #include "../../include/sgf.hpp"
 
 class Foo {
 public:
 	std::string s;
-	Foo(const std::string &s) : s(s) {}
+	Foo(const std::string &s) : s(s) {
+		std::cout << "Foo constructor called" << std::endl;
+	}
 };
+
+void some_func() {
+	std::cout << "From some_func: " << abl::resource<Foo>::get("my_resource").s << std::endl;
+}
 
 int main()
 {
-	//sgf::named_resource<Foo>::give_name("resources.cpp", "MojResurs");
-	const Foo &f = sgf::get_resource<Foo>("resources.cpp");
-	sgf::named_resource<Foo>::acquire("resources.cpp", "MyResource");
-	const Foo &f1 = sgf::get_resource<Foo>("resources.cpp");
-	
-	std::cout << "Should: " << sgf::named_resource<Foo>::is_acquired_by_name("MyResource") << std::endl;
+        abl::resource<Foo>::acquire("my_resource", Foo("One-foo"));
+        const Foo &f = abl::resource<Foo>::get("my_resource");
+        std::cout << f.s << std::endl;
 
-	std::cout << "Should not: " << sgf::named_resource<Foo>::is_acquired_by_name("MyResource") << std::endl;
-	std::cout << &f << " " << f1.s << " " <<  std::endl;
-	sgf::named_resource<Foo>::erase_by_name("MyResource");
+        try {
+        	abl::resource<Foo>::acquire("my_resource", Foo("Two-foo"));
+        } catch(const abl::resource_exception &e) {
+        	// Resource already acquired exception
+        	// could be throwed
+        	std::cout << e.what() << std::endl;
+        }
 
-	const Foo &f3 = sgf::get_resource_by_name<Foo>("MyResource");
-	std::cout << f.s << " " << f1.s << " " << f3.s << std::endl;
+        const Foo &f1 = abl::resource<Foo>::get("my_resource");
 
-	return 0;
+        std::cout << "Should not: " << abl::resource<Foo>::is_acquired("randomname") << std::endl;
+
+        std::cout << f.s << " " << f1.s << std::endl;
+        std::cout << &f << " " << &f1 << std::endl;
+
+        std::vector<std::tuple<std::string, Foo>> vec = {{"me", Foo("me")}, {"you", Foo("you")}};
+
+        for (const auto& [k, v] : vec) {
+        	try {
+        		abl::resource<Foo>::acquire(k, v);
+        	} catch (const abl::resource_exception &e) {
+        		std::cout << e.what();
+        	}
+        }
+
+        std::cout << abl::resource<Foo>::get("me").s << std::endl;
+        some_func();
+
+        /* Lazy eval */
+        abl::resource<Foo>::lazy_acquire("lazy_res", []() { return Foo("lazy_res"); });
+        std::cout << "Is acquired: " << abl::resource<Foo>::is_acquired("lazy_res") << std::endl;
+        
+        std::cout << abl::resource<Foo>::get("lazy_res").s << std::endl;
+
+        std::cout << "Is acquired: " << abl::resource<Foo>::is_acquired("lazy_res") << std::endl;
+
+      	abl::resource<Foo>::lazy_acquire("lazyone", []() {return Foo("lazyone");});
+	std::cout << "Already lazely acquired: lazyone" << std::endl;
+	std::cout << abl::resource<Foo>::get("lazyone").s << std::endl;
+	std::cout << abl::resource<Foo>::get("lazyone").s << std::endl;
+
 }
--- include/dependencies/abl/resource.hpp
+++ include/dependencies/abl/resource.hpp
@@ -1,121 +1,82 @@
-#ifndef __SGF_RESOURCE_H__
-#define __SGF_RESOURCE_H__
+#ifndef __ABL_RESOURCE_H__
+#define __ABL_RESOURCE_H__
 
-#include <filesystem>
 #include <string>
-#include <map>
+#include <unordered_map>
+#include <functional>
+#include <variant>
 
 namespace abl {
 
-template <typename T>
+template <typename value_type, typename key_type=std::string>
 class resource {
 private:
 	resource() = delete;
-	static std::map<std::filesystem::path, T> resources;
+	static std::unordered_map<key_type, std::variant<value_type, std::function<value_type(void)>>> resources;
 
 public:
-	static const T& get(const std::string &res_path);
-	static void acquire(const std::string &res_path);
-	static bool is_acquired(const std::string &res_path);
-	static void erase(const std::string &res_path);
+	static const value_type& get(const key_type &);
+	static bool is_acquired(const key_type &);
+	static void erase(const key_type &);
+	static void acquire(const key_type&, const value_type&);
+	static void lazy_acquire(const key_type&, const std::function<value_type(void)>&);
 };
 
+class resource_exception : public std::exception {
+private:
+   const std::string m_msg;
+
+public:
+	resource_exception(const std::string& msg) : m_msg(msg) { }
+	virtual const char* what() const throw () { return m_msg.c_str(); }
+};
 
 /* Initialize resources map */
-template <class T>
-std::map<std::filesystem::path, T> resource<T>::resources;
+template <typename value_type, typename key_type>
+std::unordered_map<key_type, std::variant<value_type, std::function<value_type(void)>>> resource<value_type,key_type>::resources;
 
 
-template <typename T>
-const T& resource<T>::get(const std::string &res_path) {
-	std::filesystem::path res_abs_path(std::filesystem::absolute(res_path));
-	if (!resource<T>::is_acquired(res_abs_path)) {
-		resource<T>::acquire(res_abs_path);
+template <typename value_type, typename key_type>
+const value_type& resource<value_type, key_type>::get(const key_type &key) {
+
+	if (!is_acquired(key))
+		throw resource_exception("Resource with provided key is not acquired");
+	try {
+		std::get<value_type>(resources.at(key));
+	} catch (const std::bad_variant_access&) {
+		auto f = std::get<std::function<value_type(void)>>(resources.at(key));
+		const value_type& result = std::invoke(f);
+		erase(key);
+		acquire(key, std::move(result));
 	}
-	return resource<T>::resources.at(res_abs_path);
+
+	return std::get<value_type>(resources.at(key));
 }
 
-template <typename T>
-void resource<T>::acquire(const std::string &res_path) {
-	std::filesystem::path res_abs_path(std::filesystem::absolute(res_path));
-	if (!resource<T>::is_acquired(res_abs_path)) {
-	        resources.insert(typename std::map<std::filesystem::path, T>::value_type((res_abs_path), T(res_abs_path)));
+template <typename value_type, typename key_type>
+void resource<value_type, key_type>::acquire(const key_type &key, const value_type &val) {
+	if (!is_acquired(key)) {
+	        resources.insert(typename std::unordered_map<key_type, value_type>::value_type(key, val));
+	} else {
+		throw resource_exception("Resource already acquired.");
 	}
 }
 
-template <typename T>
-bool resource<T>::is_acquired(const std::string &res_path) {
-	std::filesystem::path res_abs_path(std::filesystem::absolute(res_path));
-	if (resources.find(res_abs_path) != resources.end()) 
+template <typename value_type, typename key_type>
+void resource<value_type, key_type>::lazy_acquire(const key_type& key, const std::function<value_type(void)> &acq_func) {
+	resources.insert(typename std::unordered_map<key_type, std::function<value_type(void)>>::value_type(key, acq_func));
+}
+
+template <typename value_type, typename key_type>
+bool resource<value_type, key_type>::is_acquired(const key_type &key) {
+	if (resources.find(key) != resources.end()) 
 		return true;
 	return false;
 }
 
-template <typename T>
-void resource<T>::erase(const std::string &res_path) {
-	std::filesystem::path res_abs_path(std::filesystem::absolute(res_path));
-	resources.erase(res_abs_path);
-}
-
-
-template <class T>
-const T& get_resource(const std::string &res_path) {
-	return resource<T>::get(res_path);
-}
-
-
-template <typename T>
-class named_resource : public resource<T> {
-private:
-	named_resource() = delete;
-	static std::map<std::string, std::filesystem::path> name_path_map;
-public:
-
-	static void acquire(const std::string &res_path, const std::string &name);
-	static const T& get_by_name(const std::string &name);
-	static bool is_acquired_by_name(const std::string &name);
-	static void erase_by_name(const std::string &name);
-	static void give_name(const std::string &res_path, const std::string name);
-};
-
-/* Initialize resources map */
-template <class T>
-std::map <std::string, std::filesystem::path> named_resource<T>::name_path_map;
-
-
-template <typename T>
-void named_resource<T>::acquire(const std::string &res_path, const std::string &name) {
-	give_name(res_path, name);
-	resource<T>::acquire(res_path);
-}
-
-
-template <typename T>
-const T& named_resource<T>::get_by_name(const std::string &name) {
-	return resource<T>::get(name_path_map.at(name));
-}
-
-
-template <typename T>
-bool named_resource<T>::is_acquired_by_name(const std::string &name) {
-	return resource<T>::is_acquired(name_path_map.at(name));
-}
-
-template <typename T>
-void named_resource<T>::give_name(const std::string &res_path, const std::string name) {
-		std::filesystem::path res_abs_path(std::filesystem::absolute(res_path));
-		name_path_map[name] = res_path;
-}
-
-
-template <typename T>
-void named_resource<T>::erase_by_name(const std::string &name) {
-	resource<T>::erase(name_path_map.at(name));
-}
-
-template <class T>
-const T& get_resource_by_name(const std::string &name) {
-	return named_resource<T>::get_by_name(name);
+template <typename value_type, typename key_type>
+void resource<value_type, key_type>::erase(const key_type &key) {
+	resources.erase(key);
 }
 
 }
